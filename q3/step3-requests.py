0810ce21b8a8505894643c7721ebb0b60add7453
Justin <shinej@uchicago.edu>
2024-01-31 11:22:49 +0000

    del customheaders from step3-requests

--- q3/step3-requests.py
+++ q3/step3-requests.py
@@ -26,18 +26,6 @@ Enc_State_Repr = {
 }
 
 
-
-customheaders = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
-    "Accept-Encoding": "gzip, deflate", 
-    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8", 
-    "Dnt": "0", 
-    "Host": "uchicago.org", 
-    "Upgrade-Insecure-Requests": "1", 
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36", 
-}
-
-
 def visit_url(dest: str):
     http_reached = False    # http address reachable?
     redirect = False        # http->https redirect? <valid IFF http_access>
