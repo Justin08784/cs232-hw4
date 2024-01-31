a0400758fe8f377fa3a2fd135b2a2c93a9173784
Justin <shinej@uchicago.edu>
2024-01-31 23:31:18 +0000

    final refactoring for step3-requests

--- q3/step3-requests.py
+++ q3/step3-requests.py
@@ -29,16 +29,15 @@ def code_success(code: int):
     return 200 <= code and code <= 299
 
 def visit_url(dest: str):
+    http_reached = False
     http_success = False        # http address reachable AND return code is a success?
     redirect = False            # http->https redirect? <valid IFF http_access>
     http_code = ""
     http_accessible = False     # http address accessible? 
     
-    https_success = False       # https address reachable AND return code is a success?
-    https_accessible = False    # https address accessible? 
-    
     try:
         http_r = requests.get("http://" + dest, timeout=5)
+        http_reached = True
         
         scheme = urlparse(http_r.url).scheme
         http_code = http_r.status_code
@@ -54,19 +53,26 @@ def visit_url(dest: str):
                 exit(-1)
         
     except:
-        http_success = False
+        http_reached = False
     
-    http_accessible = http_success and not redirect
+    http_accessible = http_reached and http_success and not redirect
+    
+    
+    https_reached = False
+    https_success = False       # https address reachable AND return code is a success?
+    https_code = ""
+    https_accessible = False    # https address accessible? 
     
     try:
         https_r = requests.get("https://" + dest, timeout=5)
+        https_reached = True
         https_code = https_r.status_code
         
         https_success = code_success(https_code)
     except:
-        https_success = False
+        https_reached = False
     
-    https_accessible = https_success
+    https_accessible = https_reached and https_success
     
     state = None
     match (http_accessible, https_accessible):
@@ -79,6 +85,10 @@ def visit_url(dest: str):
         case (False, False):
             state = "neither"
     
+    # print(f"url: {dest}")
+    # print(f">> (reached: {http_reached}, success: {http_success} <code: {http_code}>, redirect: {redirect}, access: {http_accessible})")
+    # print(f">> (reached: {https_reached}, success: {https_success} <code: {https_code}>, access: {https_accessible})")
+    # print(f">> state: {state}, http_code: {http_code}")
     return state, http_code
 
 
@@ -105,7 +115,7 @@ def process_df(df: pd.DataFrame, dest_path: str):
     
 if __name__ == "__main__":
     topsites = pd.read_csv(repo_root + "/q0/step0-topsites.csv", header=None)
-    process_df(df=topsites[:10], dest_path="Zstep3-topsites-requests.csv")
+    process_df(df=topsites, dest_path="Zstep3-topsites-requests.csv")
     
     othersites = pd.read_csv(repo_root + "/q0/step0-othersites.csv", header=None)
     process_df(df=othersites[:10], dest_path="Zstep3-othersites-requests.csv")
